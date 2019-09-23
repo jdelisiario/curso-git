71f6de46bcd560212783d308b33e252c08076e64
Jaciara Elisiario <jaciaraelisiario@gmail.com>
2019-09-22 22:18:06 -0300

    Acrescentada a mensagem desejando um bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -8,8 +8,8 @@ namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
