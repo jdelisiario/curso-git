0329bf6e66ff78ce77fff702fa41e50366892263
Outro usuario <jaciaraelisiario@yahoo.com.br>
2019-09-22 22:21:07 -0300

    acrescentada a mensagem de despedida

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -10,6 +10,7 @@ namespace ProjetoTeste {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau!");
         }
     }
 }
